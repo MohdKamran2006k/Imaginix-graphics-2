243470abe26c653f94817646df3d7664f5637221
Mohd Kamran <mohdkamran88k@gmail.com>
2025-10-01 21:24:24 +0530

    Navbar Added

---
+++ app.js
@@ -0,0 +1,9 @@
+let dropdown = document.getElementById("mega-menu-full-dropdown-button");
+let navbar = document.getElementById("nav-ul");
+console.log(dropdown);
+
+function DropdownWork() {
+    console.log("clicked")
+}
+
+DropdownWork(dropdown);
