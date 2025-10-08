f2714a48270dba9165fa03cca31eb4341be7e721
Mohd Kamran <mohdkamran88k@gmail.com>
2025-10-08 23:02:02 +0530

    navbar is do something

--- app.js
+++
@@ -1,9 +0,0 @@
-let dropdown = document.getElementById("mega-menu-full-dropdown-button");
-let navbar = document.getElementById("nav-ul");
-console.log(dropdown);
-
-function DropdownWork() {
-    console.log("clicked")
-}
-
-DropdownWork(dropdown);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Inter", "sans-serif"],
+        Secondry: ['"Source Sans 3"', "sans-serif"],
+      },
+      colors: {
+        primary: "#51af74",
+        outline: "grey",
+        button: "#dc2b45",
+      },
+    },
   },
   plugins: [],
 };
