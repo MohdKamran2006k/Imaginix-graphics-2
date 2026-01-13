5c00371292e67f62b3a0748a529eb664e90f4d5e
Mohd Kamran <mohdkamran88k@gmail.com>
2026-01-13 22:51:58 +0530

    Other Html Files Setup With Tailwind Css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html","./assets/OtherPages/*.html"],
   theme: {
     extend: {
       fontFamily: {
